b5840b0b8823534f76a9eca9ba53eb6f57d1f5c1
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-05-03 17:24:39 +0700

    Initial commit

---
+++ app/src/main/java/com/si61/projectuts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.si61.projectuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
